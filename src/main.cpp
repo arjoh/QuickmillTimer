85acaa1f64accf5f5c0dccc8a1c054f6992e1978
Arjo Hooimeijer <hooimeijer@gmail.com>
2024-11-25 19:10:54 +0100

    Experiment with 128x64 OLED SSD1306

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <EEPROM.h>
+#include <Wire.h>
+#include <cmath>
+
 #include "Arduino.h"
 #include "ezButton.h"
 #include "internalLED.h"
 #include "Display.h"
-#include "Timer.h"
 
 const uint8_t LED_PIN = /*4; */ LED_BUILTIN;
 const uint8_t BUTTON_PIN = D6;
@@ -30,7 +34,7 @@ bool setting = false;
 ulong displaying = 0;
 
 // Function definitions
-void ready();
+// void ready();
 void display(ulong, uint8_t);
 void setRunning(bool);
 void checkSetting();
@@ -58,40 +62,98 @@ struct
 
 int display_timer;
 
-Display leds = Display();
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+
+Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println();
 
-  uint8_t digitPins[] = {DIGIT1_PIN, DIGIT2_PIN, DIGIT3_PIN};
-  leds.setup(LATCH_PIN, CLOCK_PIN, DATA_PIN, digitPins, 3);
+  // uint8_t digitPins[] = {DIGIT1_PIN, DIGIT2_PIN, DIGIT3_PIN};
+  // leds.setup(LATCH_PIN, CLOCK_PIN, DATA_PIN, digitPins, 3);
 
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, LED_OFF);
   button.setDebounceTime(100); // set debounce time to 50 milliseconds
 
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;
+  }
+
+  oled.clearDisplay();
+  oled.setTextColor(WHITE);
+  oled.display();
+
   readSettings();
-  ready();
+
+  oled.setCursor(0, 0);
+  oled.setTextSize(1); // 6x8
+  oled.println("QuickmillTimer");
+  oled.println();
+
+  int number = 666;
+  int decimals = 1;
+  int size = trunc(log10(number)) + 1;
+
+  int large = 6;
+  int small = 5;
+
+  uint8_t x = (128 - large * 6 * (size - decimals) - small * 6 * decimals) / 2;
+  uint8_t y = 16;
+
+  Serial.print("x: ");
+  Serial.println(x);
+  Serial.print("y: ");
+  Serial.println(y);
+
+  oled.setCursor(x, y);
+
+  Serial.print("number: ");
+  Serial.println(number);
+  Serial.print("decimals: ");
+  Serial.println(decimals);
+  Serial.print("pow(10, decimals): ");
+  Serial.println(pow(10, decimals));
+
+  int displayLarge = number / pow(10, decimals);
+  Serial.print("displayLarge: ");
+  Serial.println(displayLarge);
+  int displaySmall = number - (displayLarge * pow(10, decimals));
+  Serial.print("displaySmall: ");
+  Serial.println(displaySmall);
+
+  oled.setTextSize(large);
+  oled.print(displayLarge);
+  oled.setTextSize(small);
+  oled.println(displaySmall);
+  oled.display();
+  // ready();
 }
 
-void ready()
-{
-  // Just for fun..
-  display(666, 0);
-  leds.setBlinking(true, 2664, 666, 333);
-  while (leds.isBlinking)
-  {
-    leds.refresh();
-  }
-  // Let's get started!
-  display(seconds * 10, 1);
-}
+// void ready()
+// {
+//   // Just for fun..
+//   display(666, 0);
+//   leds.setBlinking(true, 2664, 666, 333);
+//   while (leds.isBlinking)
+//   {
+//     leds.refresh();
+//   }
+//   // Let's get started!
+//   display(seconds * 10, 1);
+// }
 
 void loop()
 {
-  leds.refresh();
+  // leds.refresh();
   checkRunning();
   checkButton();
   checkSetting();
@@ -109,7 +171,7 @@ void checkSetting()
       Serial.println("reset");
       seconds = 0;
       display(seconds, 1);
-      leds.setBlinking(true, 1000, 200, 200);
+      // leds.setBlinking(true, 1000, 200, 200);
       setting = true;
     }
     else if (setting && btnPressedFor >= settingAfter)
@@ -184,7 +246,8 @@ void display(ulong value, uint8_t decimals)
 {
   if (value != displaying)
   {
-    leds.setNumber(value, decimals);
+    // leds.setNumber(value, decimals);
+    Serial1.println(value);
     displaying = value;
   }
 }
